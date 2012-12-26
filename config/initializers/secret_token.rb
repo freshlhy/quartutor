b7913ff4ecf2fc49864544f5ec6dc3ff307449a8
liuhuyu <735077491@qq.com>
2012-12-26 16:47:05 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quartutor::Application.config.secret_token = '64da6f41515d7d7152565d1607c523aacabb52733f7acfeeee89ff98edc5bddb7a87ce6f88a75dffca0817362a854d8c1df857d564b4ec6578930f113f6ecd49'
